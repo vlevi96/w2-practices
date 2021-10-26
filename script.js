4fad243d36467407fe9be838e25940dd871d0529
vlevi96 <vlevi9609@gmail.com>
2021-10-26 15:04:35 +0200

    Keddi óra,

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function loadEvent() {
-    console.log('the page has loaded');
+function toggleMenu(event) {
+    //console.log(event.target.classList);
+    //console.log(event.target);
 
-    document.getElementById("menu-btn").addEventListener("click", clicked
-    );
+    
+    //event.target.classList.toggle("clicked");
+    const randomText="Ezt vajon kiloggolja-e?";
+
+    const fastLogResult=fastLog("Ezt a szöveget szeretném kiloggoltatni!");
+    fastLog(randomText);
+    console.log(fastLogResult);
+    document.getElementById("about").classList.add("clicked-too");
+}
+
+function loadEvent() {
+    //console.log('the page has loaded');
+
+    document.getElementById("menu-btn").addEventListener("click", toggleMenu);
+    //console.log(document.getElementById("menu-btn"));
+}
+
+function fastLog(text){
+    console.log(text);
+    return "muhahaha";
 }
 
 window.addEventListener("load", loadEvent);
-function clicked(event) {
-    console.log(event.target);
-    event.target.classList.toggle("clicked");
-}
